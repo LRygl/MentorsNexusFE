0b4f23961514c24f5c0979592b5d6680e62654fe
Lubomír Rýgl <lubomir.rygl@aevi.com>
2022-05-17 15:06:38 +0200

    CI/CD

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,11 @@ import { AuthenticationGuard } from './guard/authentication.guard';
 import { AuthenticationService } from './service/authentication.service';
 import { UserService } from './service/user.service';
 
-import { FormsModule } from '@angular/forms';
 import { AuthInterceptor } from './interceptor/auth.interceptor';
 import { LoginComponent } from './component/login/login.component';
 import { UserComponent } from './component/user/user.component';
 import { NotificationModule } from './notification.module';
 import { NotificationService } from './service/notification.service';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { FooterComponent } from './component/footer/footer.component';
 
@@ -35,8 +32,6 @@ import { FooterComponent } from './component/footer/footer.component';
     AppRoutingModule,
     HttpClientModule,
     NotificationModule,
-    NgbModule,
-    FontAwesomeModule
   ],
   providers: [
     AuthenticationService,
